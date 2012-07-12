a12211e44fe0ba822410b259c4e0132c86930edd
Simon Anders <anders@ebi.ac.uk>
2012-07-12 09:24:18 +0000

    added a experimental function for an alternative model
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DEXSeq@67588 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TRT.R
+++ R/TRT.R
@@ -1,57 +1,96 @@
-estimateDispersionsTRT <- function( ecs ) {
+estimateDispersionsTRT <- function( ecs, minCount=10, nCores=1 ) {
    stopifnot( inherits( ecs, "ExonCountSet" ) )
    stopifnot( "condition" %in% colnames( pData(ecs) ) )
    stopifnot( length( levels( pData(ecs)$condition ) ) == 2 )
    
    treated <- as.integer( pData(ecs)$condition ) == 2
    modelMatrix <- getModelMatrixTRT( rownames(pData(ecs)), treated )
+
+   testable <- rowSums(counts(ecs)) >= minCount
+   for( r in split( 1:nrow(ecs), geneIDs(ecs) ) ) {
+      if( sum( testable[r] ) <= 1 )
+         testable[r] <- FALSE
+   }
+   fData(ecs)$testable <- testable
    
-   disps <- rep( NA_real_, nrow(counts(pasillaExons)) )
-   for( i in 1:length(disps) ) 
-      try( {
-         disps[i] <- 
-            estimateOneDispersionTRT( 
-               modelMatrix, 
-               getCountVectorTRT( 
-                  ecs,  
-                  geneIDs(pasillaExons)[i],
-                  exonIDs(pasillaExons)[i] ),
-               sizeFactors(ecs) ) 
-          if(i %% 100 == 0 )
-             cat( "." ) 
-         },
-         silent=TRUE) 
-   attr( disps, "TRT" ) <- TRUE
-   fData(ecs)$dispBeforeSharing <- disps
+   if( nCores == 1 ) {
+
+      disps <- rep( NA_real_, nrow(counts(ecs)) )
+      for( i in 1:length(disps) ) 
+         if( fData(ecs)$testable[i] )
+            try( {
+               disps[i] <- 
+                  estimateOneDispersionTRT( 
+                     modelMatrix, 
+                     getCountVectorTRT( 
+                        ecs,  
+                        geneIDs(ecs)[i],
+                        exonIDs(ecs)[i] ),
+                     sizeFactors(ecs) ) 
+                if(i %% 100 == 0 )
+                   cat( "." ) 
+               },
+            silent=FALSE) 
+      attr( disps, "TRT" ) <- TRUE
+      fData(ecs)$dispBeforeSharing <- disps
+
+   } else {
+
+      testableGenes <- as.character(unique(fData(ecs)[which(fData(ecs)$testable),]$geneID))
+      ecs <- divideWork( 
+         ecs, 
+         funtoapply = estimateDispersionsTRT,
+         fattr = "dispBeforeSharing", 
+         mc.cores = nCores, 
+         testableGenes )
+
+   }
    ecs
 }
 
-testForDEU.TRT <- function( ecs ) {
+testForDEU.TRT <- function( ecs, nCores=1 ) {
    stopifnot( inherits( ecs, "ExonCountSet" ) )
    if( all(is.na(featureData(ecs)$dispersion)) )
       stop("No dispersion values found, call function fitDispersionFunction first.")
    
-   sf <- rep( sizeFactors( pasillaExons ), 2 )
+   sf <- rep( sizeFactors( ecs ), 2 )
    disps <- fData(ecs)$dispersion
    treated <- as.integer( pData(ecs)$condition ) == 2
    mm1 <- getModelMatrixTRT( rownames(pData(ecs)), treated )
    mm0 <- mm1[,-ncol(mm1)]
    
-   pvals <- rep( NA_real_, nrow(counts(pasillaExons)) )
-   for( i in 1:length(pvals) ) {
-      cv <- getCountVectorTRT( ecs, geneIDs(ecs)[i], exonIDs(ecs)[i] )
-      try( 
-         {
-            fit1 <- statmod::glmnb.fit( mm1, cv, disps[i], log(sf) )
-            fit0 <- statmod::glmnb.fit( mm0, cv, disps[i], log(sf) )
-            pvals[i] <- 1 - pchisq( deviance(fit0) - deviance(fit1), 1 )
-         if(i %% 100 == 0 )
-             cat( "." ) 
-         },
-         silent=TRUE)
-   }
+   if( nCores == 1 ) {
+
+      pvals <- rep( NA_real_, nrow(counts(ecs)) )
+      for( i in 1:length(pvals) ) 
+         if( fData(ecs)$testable[i] ) {
+            cv <- getCountVectorTRT( ecs, geneIDs(ecs)[i], exonIDs(ecs)[i] )
+            try( 
+               {
+                  fit1 <- statmod::glmnb.fit( mm1, cv, disps[i], log(sf) )
+                  fit0 <- statmod::glmnb.fit( mm0, cv, disps[i], log(sf) )
+                  pvals[i] <- 1 - pchisq( deviance(fit0) - deviance(fit1), 1 )
+               if(i %% 100 == 0 )
+                   cat( "." ) 
+               },
+               silent=TRUE)
+         }
+      fData(ecs)$pvalue <- pvals
    
-   fData(ecs)$pvalue <- pvals
+   } else{
+
+      testableGenes <- as.character(unique(fData(ecs)[which(fData(ecs)$testable),]$geneID))
+      ecs <- divideWork( 
+         ecs, 
+         funtoapply = testForDEU.TRT,
+         fattr = "pvalue", 
+         mc.cores = nCores, 
+         testableGenes )
+
+   }
+
+   fData(ecs)$padjust <- p.adjust( fData(ecs)$pvalue, method="BH" )
+
    ecs
 }
 
