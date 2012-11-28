f356f60bf403a849dc74b67b1d450fcac0997ab4
Alejandro Reyes <alejandro.reyes@embl.de>
2012-11-28 12:25:25 +0000

    TRT implemented
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DEXSeq@71517 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TRT.R
+++ R/TRT.R
@@ -1,101 +1,23 @@
-estimateDispersionsTRT <- function( ecs, minCount=10, nCores=1 ) {
-   stopifnot( inherits( ecs, "ExonCountSet" ) )
-   stopifnot( "condition" %in% colnames( pData(ecs) ) )
-   stopifnot( length( levels( pData(ecs)$condition ) ) == 2 )
-
-   treated <- as.integer( pData(ecs)$condition ) == 2
-   modelMatrix <- getModelMatrixTRT( rownames(pData(ecs)), treated )
-
-   testable <- rowSums(counts(ecs)) >= minCount
-   for( r in split( seq_len(nrow(ecs)), geneIDs(ecs) ) ) {
-      if( sum( testable[r] ) <= 1 )
-         testable[r] <- FALSE
-   }
-   fData(ecs)$testable <- testable
-
-   if( nCores == 1 ) {
-
-      disps <- rep( NA_real_, nrow(counts(ecs)) )
-      for( i in seq(along=disps) )
-         if( fData(ecs)$testable[i] )
-            try( {
-               disps[i] <-
-                  estimateOneDispersionTRT(
-                     modelMatrix,
-                     getCountVectorTRT(
-                        ecs,
-                        geneIDs(ecs)[i],
-                        exonIDs(ecs)[i] ),
-                     sizeFactors(ecs) )
-                if(i %% 100 == 0 )
-                   cat( "." )
-               },
-            silent=FALSE)
-      attr( disps, "TRT" ) <- TRUE
-      fData(ecs)$dispBeforeSharing <- disps
-
-   } else {
-
-      testableGenes <- as.character(unique(fData(ecs)[which(fData(ecs)$testable),]$geneID))
-      ecs <- divideWork(
-         ecs,
-         funtoapply = estimateDispersionsTRT,
-         fattr = "dispBeforeSharing",
-         mc.cores = nCores,
-         testableGenes )
-
-   }
-   ecs
+rmDepCols <- function(m) {
+   q <- qr( m )
+   if( q$rank < ncol(m) )
+      m[ , -q$pivot[ (q$rank+1) : ncol(m) ] ]
+   else
+      m
 }
 
-testForDEU.TRT <- function( ecs, nCores=1 ) {
-   stopifnot( inherits( ecs, "ExonCountSet" ) )
-   if( all(is.na(featureData(ecs)$dispersion)) )
-      stop("No dispersion values found, call function fitDispersionFunction first.")
-
-   sf <- rep( sizeFactors( ecs ), 2 )
-   disps <- fData(ecs)$dispersion
-   treated <- as.integer( pData(ecs)$condition ) == 2
-   mm1 <- getModelMatrixTRT( rownames(pData(ecs)), treated )
-   mm0 <- mm1[,-ncol(mm1)]
-
-   if( nCores == 1 ) {
-
-      pvals <- rep( NA_real_, nrow(counts(ecs)) )
-      for( i in seq(along=pvals) )
-         if( fData(ecs)$testable[i] ) {
-            cv <- getCountVectorTRT( ecs, geneIDs(ecs)[i], exonIDs(ecs)[i] )
-            try(
-               {
-                  fit1 <- statmod::glmnb.fit( mm1, cv, disps[i], log(sf) )
-                  fit0 <- statmod::glmnb.fit( mm0, cv, disps[i], log(sf) )
-                  pvals[i] <- 1 - pchisq( deviance(fit0) - deviance(fit1), 1 )
-               if(i %% 100 == 0 )
-                   cat( "." )
-               },
-               silent=TRUE)
-         }
-      fData(ecs)$pvalue <- pvals
-
-   } else{
-
-      testableGenes <- as.character(unique(fData(ecs)[which(fData(ecs)$testable),]$geneID))
-      ecs <- divideWork(
-         ecs,
-         funtoapply = testForDEU.TRT,
-         fattr = "pvalue",
-         mc.cores = nCores,
-         testableGenes )
-
-   }
-
-   fData(ecs)$padjust <- p.adjust( fData(ecs)$pvalue, method="BH" )
-
-   ecs
+modelFrameForTRT <- function( ecs ){
+  stopifnot( inherits( ecs, "ExonCountSet" ) )
+  modelFrame <- cbind(
+     sample = sampleNames(ecs),
+     design(ecs),
+     sizeFactor = sizeFactors(ecs) )
+  modelFrame <- rbind(
+     cbind( modelFrame, exon="this" ),
+     cbind( modelFrame, exon="others" ) )
+  rownames(modelFrame) <- NULL
+  return( modelFrame )
 }
-
-
-# internal functions
 
 getCountVectorTRT <- function( ecs, geneID, exonID ) {
    stopifnot( inherits( ecs, "ExonCountSet" ) )
@@ -105,35 +27,134 @@ getCountVectorTRT <- function( ecs, geneID, exonID ) {
    c( ctfg[ w, ], colSums( ctfg[ -w, , drop=FALSE ] ) )
 }
 
-getModelMatrixTRT <- function( sampleNames, treated ) {
-   mf <- data.frame(
-      sample = rep( sampleNames, 2 ),
-      treated = rep( ifelse( treated, "yes", "no" ), 2 ),
-      what = rep( c( "this", "others" ), each = length(sampleNames) ) )
-
-   mm <- model.matrix( ~ 0 + sample + what + treated:what, mf )
-   mm <- mm[ , colnames(mm) != "whatothers:treatedyes" ]
-   rownames(mm) <- paste(
-      rep( sampleNames, 2),
-      rep( c( "this", "other" ), each = length(sampleNames) ),
-      sep = ":" )
-   mm
+estimateExonDispersionTRT <- function( ecs, geneID, exonID, modelFrame, mm ){
+   stopifnot( inherits( ecs, "ExonCountSet" ) )
+   if( all( is.na( sizeFactors( ecs ) )) ){
+     stop("Please calculate size factors before estimating dispersions\n")
+   }
+   
+   count <- getCountVectorTRT( ecs, geneID, exonID )
+   disp <- .1
+   for( i in 1:10 ) {
+     fit <- glmnb.fit( mm, count, disp, log( modelFrame$sizeFactor ) )
+     olddisp <- disp
+     disp <- exp( optimize( function(logalpha)
+     profileLogLikelihood( exp(logalpha), mm, count, fitted.values(fit) ), log( c( 1e-11, 1e5 ) ), maximum=TRUE, tol=.01 )$maximum )
+     if( abs( log(disp) - log(olddisp) ) < .03 )
+        break
+     }
+  disp
 }
 
-estimateOneDispersionTRT <- function( modelMatrix, countVector,
-      sizeFactors, dispInitialGuess = .5 ) {
+estimateDispersionsTRT <- function( ecs, formula= ~ sample + condition * exon, nCores=1, minCount=10 ){
+  stopifnot( inherits( ecs, "ExonCountSet" ) )
+   if( all( is.na( sizeFactors( ecs )) ) ){
+     stop("Please calculate size factors before estimating dispersions\n")
+   }
+   testable <- rowSums(counts(ecs)) >= minCount
+   for( r in split( seq_len(nrow(ecs)), geneIDs(ecs) ) ) {
+     if( sum( testable[r] ) <= 1 )
+       testable[r] <- FALSE
+   }
+   fData(ecs)$testable <- testable
 
-   fit1 <- statmod::glmnb.fit( modelMatrix, countVector,
-                      dispInitialGuess, rep( log(sizeFactors), 2 ) )
+   if(!all(testable) & nCores<=1){
+      warning(sprintf("Exons with less than %d counts will not be tested. For more details please see the manual page of 'estimateDispersions', parameter 'minCount'", minCount))
+   }
 
-   exp( optimize(
-      function( logalpha )
-         -profileLogLikelihood(
-            exp(logalpha),
-            modelMatrix,
-            countVector,
-            fitted.values(fit1) ),
-      log( c( 1e-5, 1e3 ) ) )$minimum )
+   if( nCores > 1 ){
+      if (!is.loaded("mc_fork", PACKAGE = "parallel")) {
+          stop("Please load first parallel package or set parameter nCores to 1...")
+      }else{
+          myApply <- function(X, FUN){ parallel:::mclapply( X, FUN, mc.cores=nCores ) }
+      }
+   }else{
+     myApply <- lapply
+   }
+   ecs@formulas[["formulaDispersionTRT"]] <- deparse(formula)
+
+   rows <- seq_len(nrow(ecs))
+   modelFrame <- modelFrameForTRT( ecs )
+   mm <- rmDepCols( model.matrix( formula, modelFrame ) )
+   disps <- myApply( rows, function(i) {
+      if( i %% 100 == 0 )
+         cat(".")
+      if( fData(ecs)$testable[i] ) {
+         a <- try( estimateExonDispersionTRT( ecs, geneIDs(ecs)[i], exonIDs(ecs)[i], modelFrame, mm ) )
+         if( inherits( a, "try-error" ) ) {
+            warning( sprintf("Unable to estimate dispersions for %s:%s", as.character( geneIDs(ecs)[i] ), exonIDs(ecs)[i]) )
+            NA }
+         else{
+            a }
+      }else{
+          NA 
+      }
+    })
+    names(disps) <- featureNames(ecs)
+    fData(ecs)[names(disps), "dispBeforeSharing"] <- unlist(disps)
+    fData(ecs)$testable[which( is.na( fData(ecs)$dispBeforeSharing ) )] <- FALSE
+    cat("\nDone\n")
+    ecs
 }
 
-#profileLogLikelihood <- DEXSeq:::profileLogLikelihood
+
+testExonForDEUTRT <- function(ecs, geneID, exonID, modelFrame, mm0, mm1, disp){
+  stopifnot( inherits( ecs, "ExonCountSet" ) )
+  stopifnot( !is.na(disp) )
+  stopifnot( any(geneIDs(ecs) %in% geneID & exonIDs(ecs) %in% exonID ) )
+  if( all( is.na( sizeFactors( ecs )) ) ){
+    stop("Please calculate size factors first\n")
+  }
+  count <- getCountVectorTRT( ecs, geneID, exonID )
+  fit0  <- glmnb.fit( mm0,  count, disp, log( modelFrame$sizeFactor ) )
+  fit1 <- glmnb.fit( mm1, count, disp, log( modelFrame$sizeFactor ) )
+  pchisq( deviance( fit0 ) - deviance( fit1 ), ncol( mm1 ) - ncol( mm0 ), lower.tail=FALSE )
+}
+
+testForDEUTRT <- function( ecs, nCores=1, formula0= ~sample + condition + exon, formula1= ~sample + condition * exon, dispColumn="dispersion"){
+  stopifnot( inherits( ecs, "ExonCountSet" ) )
+   if( all( is.na( sizeFactors( ecs )))) {
+     stop("Please calculate size factors before estimating dispersions\n")
+   } 
+   if( all( is.na( fData(ecs)[,dispColumn] ) ) ){
+     stop("Please estimate dispersions before calling this function\n")
+   }
+   if( nCores > 1 ){
+      if (!is.loaded("mc_fork", PACKAGE = "parallel")) {
+          stop("Please load first parallel package or set parameter nCores to 1...")
+      }else{
+          myApply <- function(X, FUN){ parallel:::mclapply( X, FUN, mc.cores=nCores ) }
+      }
+   }else{
+     myApply <- lapply
+   }
+   ecs@formulas[["formula0TRT"]] <- deparse(formula0)
+   ecs@formulas[["formula1TRT"]] <- deparse(formula1)
+   rows <- seq_len(nrow(ecs))
+   modelFrame <- modelFrameForTRT( ecs )
+   mm0 <- rmDepCols( model.matrix( formula0, modelFrame ) )
+   mm1 <- rmDepCols( model.matrix( formula1, modelFrame ) )
+
+   pvals <- myApply( rows,
+     function(i) {
+       if( i %% 1000 == 0 )
+          cat(".")
+       if( fData(ecs)$testable[i] ) {
+          a <- try( testExonForDEUTRT( ecs, geneIDs(ecs)[i], exonIDs(ecs)[i], modelFrame, mm0, mm1, fData(ecs)[i, dispColumn] ) )
+          if( any(inherits( a, "try-error" ) )) {
+             warning( sprintf("Unable to calculate p-values for %s:%s\n", as.character( geneIDs(ecs)[i] ), exonIDs(ecs)[i]) )
+             NA }
+          else
+             a }
+       else
+          NA })
+
+    names( pvals ) <- featureNames( ecs )
+    fData(ecs)[names(pvals), "pvalue"] <- unlist(pvals)
+    fData(ecs)$padjust <- p.adjust( fData(ecs)$pvalue, method="BH" )
+    ecs
+
+}
+
+
+
